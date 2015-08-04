0fcbe7c70227b13e4bad52eeb60751aba2c516e0
staceyko <ko.stacey@gmail.com>
2015-08-04 16:32:30 -0400

    made changes

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,23 +1,23 @@
-require_relative './box.rb'
+#require_relative './box.rb'
 
 class Cat
     attr_accessor :name, :size
     def meow
         puts "meow!"
     end
-
-    def get_in_box(box_size)
-        if self.size <= box_size
-        puts "Meow! I'm in the box!"
-        else
-        puts "Sad meow, I'm not in box"
-        end
+#
+#    def get_in_box(box_size)
+#        if self.size <= box_size
+#        puts "Meow! I'm in the box!"
+#        else
+#        puts "Sad meow, I'm not in box"
+#        end
 end
-
-maru = Cat.new
-maru.size = 2
-
-fedex_box = Box.new
-fedex_box.size = 3
-
-maru.get_in_box(fedex_box)
+#
+#maru = Cat.new
+#maru.size = 2
+#
+#fedex_box = Box.new
+#fedex_box.size = 3
+#
+#maru.get_in_box(fedex_box)
